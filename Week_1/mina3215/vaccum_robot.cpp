03246e02d64fbb4b7da2bc22da713b696ca3d043
kirua12 <kch8448ch@naver.com>
2026-03-23 22:06:39 +0900

    base

--- Week_1/mina3215/vaccum_robot.cpp
+++ Week_1/mina3215/vaccum_robot.cpp
@@ -5,5 +5,25 @@
 
 int main()
 {
-    
+    int N = 0;
+
+    std::cin >> N;
+
+    int **room = new int*[N]; // 방의 상태를 입력 받은 만큼 할당함 2차원 배열로
+
+    for (int i = 0; i < N; i++) {
+        room[i] = new int[N];
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            std::string input;
+            std::cin >> input;
+            room[i][j] = std::stoi(input); //입력받은 방의 상태를 정수로 변환하여 배열에 저장
+        }
+    }
+
+
 }
