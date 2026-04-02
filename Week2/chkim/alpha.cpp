3fa7b2c46df80616565b8ff18658960e0d6aa1f3
kirua12 <kch8448ch@naver.com>
2026-04-03 08:26:07 +0900

    update alpha complete

--- Week2/chkim/alpha.cpp
+++ Week2/chkim/alpha.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string alpha = "abcdefghijklmnopqrstuvwxyz";
+    int checker_alpha[26] = {0};
+
+    std::string input;
+
+    std::cin >> input;
+
+    for(int i=0; i<input.size();i++)
+    {   
+        for(int j= 0; j<26;j++)
+        {
+            if(input.c_str()[i]==alpha.c_str()[j])
+            {
+                checker_alpha[j]++;
+                break;
+            }
+        }
+    }
+
+    for(int i=0; i<26; i++)
+    {
+        std::cout<< checker_alpha[i]<<" ";
+    }
+
+
+
+
+}
